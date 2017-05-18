13c4314a6daf9946b9030ce3717984d897fac006
weige <yanwei@geetest.com>
2017-05-18 18:15:14 +0800

    demo2

---
+++ chapter3/3-4.py
@@ -0,0 +1,8 @@
+t=1
+while(t):
+    a=float(raw_input('Please input a number btween 1 and 100...'))
+    if 1<a<100:
+        print 'Your have input a number between 1 and 100..'
+        t=0
+    else :
+        print 'Are you a big SB...'
