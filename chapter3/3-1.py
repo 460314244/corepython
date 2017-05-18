503b185ace44dbc32bcec279902692f6202bb105
weige <yanwei@geetest.com>
2017-05-18 15:39:31 +0800

    i finished

--- chapter3/3-1.py
+++ chapter3/3-1.py
@@ -1,5 +1,9 @@
-h = 0
-for a in range(100):
-    h = h + a + 1
-print h
-123213
+a=raw_input("Please input a string ...")
+print 'Display in for loop:'
+for i in a:
+    print i
+print '\nDisplay inwhile loop:'
+j=0
+while (j<len(a)):
+    print a[j]
+    j=j+1
